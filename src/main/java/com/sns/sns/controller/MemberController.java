1717a09432b5186e4393607cab6ee7dd2e6c1de9
WoojinDeve <jwj060111@gmail.com>
2022-11-09 15:49:09 +0900

    feat: 회원 정보 저장 컨트롤러 구현

---
+++ src/main/java/com/sns/sns/controller/MemberController.java
@@ -0,0 +1,20 @@
+package com.sns.sns.controller;
+
+import com.sns.sns.domain.member.dto.RegisterMemberCommand;
+import com.sns.sns.domain.member.entity.Member;
+import com.sns.sns.domain.member.service.MemberWriteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberWriteService memberWriteService;
+
+    @PostMapping("/members")
+    public Member register(@RequestBody RegisterMemberCommand command){
+        return memberWriteService.create(command);
+    }
+}

984dac893208a2cf9684a09f2bdafc76715c38f5
WoojinDeve <jwj060111@gmail.com>
2022-11-09 19:48:44 +0900

    refactor: 테스트 데이터 팩토리 메서드명 변경에 따른 수정

--- src/test/java/com/sns/sns/domain/member/entity/MemberNicknameHistoryTest.java
+++ src/test/java/com/sns/sns/domain/member/entity/MemberNicknameHistoryTest.java
@@ -12,7 +12,7 @@ class MemberNicknameHistoryTest {
     @Test
     @DisplayName("엔티티가 정삭적으로 생성된다.")
     void 엔티티가_정상적으로_생성된다() {
-        assertDoesNotThrow(() -> MemberNickNameHistoryFixtures.createMember());
+        assertDoesNotThrow(MemberNickNameHistoryFixtures::createMemberNicknameHistory);
     }
 
     @Test

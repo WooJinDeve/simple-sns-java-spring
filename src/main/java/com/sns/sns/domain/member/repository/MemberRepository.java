e52ef4439c98d1a4a38e71de033e8a57e0b3a051
WoojinDeve <jwj060111@gmail.com>
2022-11-09 16:49:46 +0900

    feat: 회원 레포지토리 단건조회 기능

--- src/main/java/com/sns/sns/domain/member/repository/MemberRepository.java
+++ src/main/java/com/sns/sns/domain/member/repository/MemberRepository.java
@@ -1,21 +1,29 @@
 package com.sns.sns.domain.member.repository;
 
 import com.sns.sns.domain.member.entity.Member;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Repository
 public class MemberRepository {
+    private static final String TABLE = "Member";
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private final SimpleJdbcInsert simpleJdbcInsert;
 
     public MemberRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
         this.simpleJdbcInsert = new SimpleJdbcInsert(this.namedParameterJdbcTemplate.getJdbcTemplate())
-                .withTableName("Member")
+                .withTableName(TABLE)
                 .usingGeneratedKeyColumns("id");
     }
 
@@ -44,5 +52,28 @@ public class MemberRepository {
     private Member update(Member member){
         // TODO: implemented
         return member;
+    }
+
+    /*
+    * select *
+    * from Member
+    * where id = :id
+    * */
+    public Optional<Member> findById(Long id){
+        var sql = String.format("SELECT * FROM %s WHERE id = :id", TABLE);
+        var param = new MapSqlParameterSource()
+                .addValue("id", id);
+
+        RowMapper<Member> rowMapper = (ResultSet resultSet, int rowNum) -> Member
+                .builder()
+                .id(resultSet.getLong("id"))
+                .email(resultSet.getString("email"))
+                .nickname(resultSet.getString("nickname"))
+                .birthday(resultSet.getObject("birthday", LocalDate.class))
+                .createdAt(resultSet.getObject("createdAt", LocalDateTime.class))
+                .build();
+
+        Member member = namedParameterJdbcTemplate.queryForObject(sql, param, rowMapper);
+        return Optional.ofNullable(member);
     }
 }

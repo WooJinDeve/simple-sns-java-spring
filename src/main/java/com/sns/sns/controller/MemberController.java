216920087f280346a9673cdb9a62eb58907b1e5f
WoojinDeve <jwj060111@gmail.com>
2022-11-09 19:32:15 +0900

    feat: 회원의 닉네임 변경사항 기록 조회 컨트롤러 코드 구현

--- src/main/java/com/sns/sns/controller/MemberController.java
+++ src/main/java/com/sns/sns/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.sns.sns.controller;
 
 import com.sns.sns.domain.member.dto.MemberDto;
+import com.sns.sns.domain.member.dto.MemberNicknameHistoryDto;
 import com.sns.sns.domain.member.dto.RegisterMemberCommand;
 import com.sns.sns.domain.member.entity.Member;
 import com.sns.sns.domain.member.service.MemberReadService;
@@ -8,20 +9,34 @@ import com.sns.sns.domain.member.service.MemberWriteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
     private final MemberWriteService memberWriteService;
     private final MemberReadService memberReadService;
 
-    @PostMapping("/members")
+    @PostMapping
     public MemberDto register(@RequestBody RegisterMemberCommand command){
         Member member = memberWriteService.create(command);
         return memberReadService.toDto(member);
     }
 
-    @GetMapping("/members/{id}")
+    @GetMapping("/{id}")
     public MemberDto getMember(@PathVariable Long id) {
         return memberReadService.getMember(id);
+    }
+
+    @PostMapping("/{id}/name")
+    public MemberDto changeNickname(@PathVariable Long id, @RequestBody String nickname){
+        memberWriteService.changeNickname(id, nickname);
+        return memberReadService.getMember(id);
+    }
+
+    @GetMapping("/{memberId}/nickname-histories")
+    public List<MemberNicknameHistoryDto> getNicknameHistories(@PathVariable Long memberId) {
+        return memberReadService.getNicknameHistories(memberId);
     }
 }

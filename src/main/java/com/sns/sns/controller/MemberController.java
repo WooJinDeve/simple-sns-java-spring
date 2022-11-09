e026b17a09e8a0cf794a3b6239611136831013d4
WoojinDeve <jwj060111@gmail.com>
2022-11-09 16:50:11 +0900

    feat: 회원 컨트롤러 단건조회 기능

--- src/main/java/com/sns/sns/controller/MemberController.java
+++ src/main/java/com/sns/sns/controller/MemberController.java
@@ -2,19 +2,24 @@ package com.sns.sns.controller;
 
 import com.sns.sns.domain.member.dto.RegisterMemberCommand;
 import com.sns.sns.domain.member.entity.Member;
+import com.sns.sns.domain.member.service.MemberReadService;
 import com.sns.sns.domain.member.service.MemberWriteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberWriteService memberWriteService;
+    private final MemberReadService memberReadService;
 
     @PostMapping("/members")
     public Member register(@RequestBody RegisterMemberCommand command){
         return memberWriteService.create(command);
+    }
+
+    @GetMapping("/members/{id}")
+    public Member getMember(@PathVariable Long id) {
+        return memberReadService.getMember(id);
     }
 }

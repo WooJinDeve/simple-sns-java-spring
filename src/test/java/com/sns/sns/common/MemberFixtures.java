3f5259409b35140baf8d429eea761092fb3410c5
WoojinDeve <jwj060111@gmail.com>
2022-11-09 18:42:44 +0900

    feat: eszyRandom을 통한 랜덤한 데이터 팩토리 생성

--- src/test/java/com/sns/sns/common/MemberFixtures.java
+++ src/test/java/com/sns/sns/common/MemberFixtures.java
@@ -2,6 +2,8 @@ package com.sns.sns.common;
 
 import com.sns.sns.domain.member.dto.RegisterMemberCommand;
 import com.sns.sns.domain.member.entity.Member;
+import org.jeasy.random.EasyRandom;
+import org.jeasy.random.EasyRandomParameters;
 
 import java.time.LocalDate;
 
@@ -11,15 +13,27 @@ public class MemberFixtures {
     public static final String 닉네임 = "닉네임";
     public static final LocalDate 생일 = LocalDate.now();
 
-    public static RegisterMemberCommand createMemberCommand(){
+    public static RegisterMemberCommand createMemberCommand() {
         return new RegisterMemberCommand(이메일, 닉네임, 생일);
     }
 
-    public static Member createMember(){
+    public static Member createBuilderMember() {
         return Member.builder()
                 .email(이메일)
                 .nickname(닉네임)
                 .birthday(생일)
                 .build();
     }
+
+    public static Member createMember() {
+        var param = new EasyRandomParameters();
+        return new EasyRandom(param).nextObject(Member.class);
+    }
+
+    public static Member createMember(Long seed) {
+        var param = new EasyRandomParameters().seed(seed);
+        return new EasyRandom(param).nextObject(Member.class);
+    }
+
+
 }

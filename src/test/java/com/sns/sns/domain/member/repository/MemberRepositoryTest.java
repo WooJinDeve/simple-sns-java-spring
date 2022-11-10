c1dd6d4fc71be6dcce9e569f0783d82a94223823
WoojinDeve <jwj060111@gmail.com>
2022-11-10 09:31:08 +0900

    feat: 회원 전체조회 테스트

--- src/test/java/com/sns/sns/domain/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/sns/sns/domain/member/repository/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.sns.sns.common.MemberFixtures.*;
@@ -58,5 +59,48 @@ class MemberRepositoryTest extends RepositoryTest {
 
         //then
         assertThat(actual.isPresent()).isFalse();
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 회원아이디로 전체 조회시 빈 리스트를 반환한다.")
+    void 존재하지않는_회원아이디로_전체_조회시_빈_리스트를_반환한다(){
+        //given
+        List<Long> ids = List.of();
+
+        //when
+        List<Member> actual = memberRepository.findAllByIdIn(ids);
+
+        //then
+        assertThat(actual.size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("회원 아이디가 저장된 리스트로 전체 조회를 한다.")
+    void 회원_아이디가_저장된_리스트로_전체조회를_한다(){
+        //given
+        Long id1 = memberRepository.save(createBuilderMember()).getId();
+        Long id2 = memberRepository.save(createBuilderMember()).getId();
+        List<Long> ids = List.of(id1, id2);
+
+        //when
+        List<Member> actual = memberRepository.findAllByIdIn(ids);
+
+        //then
+        assertThat(actual.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("존재하는 회원 아이디와 존재하지 않는 회원 아이디로 전체조회를 했을때 존재하는 회원만 반환된다.")
+    void 존재하는_회원_아이디와_존재하지_않는_회원_아이디로_전체조회를_했을때_존재하는_회원만_반환된다(){
+        //given
+        Long id1 = memberRepository.save(createBuilderMember()).getId();
+        Long id2 = 0L;
+        List<Long> ids = List.of(id1, id2);
+
+        //when
+        List<Member> actual = memberRepository.findAllByIdIn(ids);
+
+        //then
+        assertThat(actual.size()).isEqualTo(1);
     }
 }
